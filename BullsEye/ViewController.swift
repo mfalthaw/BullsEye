b0547ba5006252277de13a2512649609827cd222
Moath <mfalthaw@ucalgary.ca>
2019-02-06 18:42:48 +0300

    coding basics done, bonus points

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
     var currentValue = 0
     var targetValue = 0
     var score = 0
+    var rounds = 0
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var roundsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +27,27 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
-        let points = (100 - difference)
+        var points = 100 - difference
+        let alertTitle: String
+        
+        if(difference == 0){
+            alertTitle = "Perfect!"
+            points += 100
+        } else if (points > 95){
+            alertTitle = "You almost had it!"
+            if(difference == 1){
+                points += 50
+            }
+        } else if (points > 90){
+            alertTitle = "Pretty good!"
+        } else {
+            alertTitle = "Not even close!"
+        }
         score += points
     
         
         let message = "You scored \(points) points!"
-        
-        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: alertTitle, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         
         alert.addAction(action)
@@ -48,12 +64,14 @@ class ViewController: UIViewController {
         targetValue = Int.random(in: 1...100)
         currentValue = 50
         slider.value = Float(currentValue)
+        rounds += 1
         updateLabels()
     }
     
     func updateLabels(){
         targetLabel.text = String(targetValue)
         pointsLabel.text = String(score)
+        roundsLabel.text = String(rounds)
     }
 }
 
