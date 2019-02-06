ddb6724cdaef7c591a6c788c1138b7ce1d3457fe
Moath <mfalthaw@ucalgary.ca>
2019-02-06 15:28:55 +0300

    started main view, print slider value

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World!", message: String(currentValue), preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = Int(slider.value)
+        print("The value of the slider is now: \(currentValue)")
+    }
 }
 
